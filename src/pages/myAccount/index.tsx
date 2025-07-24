018ed1f1732d0c5d741ee4e6234e1eb2b1176079
annae <anna.romagosa@smartassets.ai>
2025-07-24 12:32:05 +0200

    myaccount redirects to profile

--- src/pages/myAccount/index.tsx
+++ src/pages/myAccount/index.tsx
@@ -1,14 +1,19 @@
 import { useAuthContext } from '@/auth/provider/useAuthContext'
 import BaseLayout from '@/common/layouts/baseLayout'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { SideMenu } from './components/sideMenu'
 import FlexContainer from '@/components/flexContainer'
 import { tokens } from '@/variables/styles'
+import { routeMap } from '@/routes/utils'
 
 const MyAccountPage = () => {
   const { isAuthenticated } = useAuthContext()
 
+  const location = useLocation()
+
   if (!isAuthenticated) return <div>You must login to access this page</div>
+
+  if (location.pathname === routeMap.myAccount.path) return <Navigate to={routeMap.profile.path} replace />
 
   return (
     <BaseLayout>
