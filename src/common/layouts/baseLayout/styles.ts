1bb279fb23aebb029876bd73a2632bc79921c978
Anna Esteve <aesteve8@gmail.com>
2025-10-02 11:31:58 +0200

    small fixes (#27) (#28)

--- src/common/layouts/baseLayout/styles.ts
+++ src/common/layouts/baseLayout/styles.ts
@@ -6,23 +6,33 @@ import type { IBaseLayoutProps } from './types'
 import { media } from '@/theme'
 import { css } from 'styled-components'
 
+const mobilePadding = css`
+  padding: 0 ${tokens.space.lg};
+`
+
+const tabletPadding = css`
+  padding: 0 ${tokens.space.xl3};
+`
+
+const desktopPadding = css`
+  padding: 0 ${tokens.space.xl6};
+`
+
 const StyledBaseLayout = styled(FlexContainer)<Pick<IBaseLayoutProps, 'isHome'>>`
   min-height: 100%;
-
-  padding: ${({ isHome }) => (isHome ? '0' : `0 ${tokens.space.xl6}`)};
-  ${({ isHome }) =>
-    media.mobile(
-      isHome
-        ? css`
-            padding: 0;
-          `
-        : css`
-            padding: 0 ${tokens.space.lg};
-          `
-    )}
-
   max-width: ${MAX_VIEW_WIDTH};
   margin: 0 auto;
+
+  ${({ isHome }) =>
+    isHome
+      ? css`
+          padding: 0;
+        `
+      : css`
+          ${media.mobile(mobilePadding)}
+          ${media.tablet(tabletPadding)}
+          ${media.desktop(desktopPadding)}
+        `}
 `
 
 export { StyledBaseLayout }
